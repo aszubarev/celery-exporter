63d8617ed887b643d75b069fd54c2594a7b80b43
aszubarev <aszubarev@domclick.ru>
2024-04-07 18:17:08 +0300

    init

--- examples/producer.py
+++ examples/producer.py
@@ -1,16 +1,17 @@
 from time import sleep
 
-import tasks
+import service_default
+
 from celery.result import AsyncResult
 
-tasks.test_succeeded.delay()
-tasks.test_failed.delay()
-tasks.test_reject_with_no_requeue.delay()
-tasks.test_retried.delay()
+service_default.test_succeeded.delay()
+service_default.test_failed.delay()
+service_default.test_reject_with_no_requeue.delay()
+service_default.test_retried.delay()
 
-result: AsyncResult = tasks.test_revoked.apply_async(countdown=5)
+result: AsyncResult = service_default.test_revoked.apply_async(countdown=5)
 print(result.id)
 sleep(1)
 print(f'{result.id} revoke')
-tasks.app.control.revoke(result.id)
+service_default.app.control.revoke(result.id)
 
